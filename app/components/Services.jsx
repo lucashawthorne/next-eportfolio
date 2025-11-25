5307f0c49796db955596ae952ac087c98ab6fb76
Raindomize <lucashpro17@gmail.com>
2025-11-25 10:08:30 -0700

    implement initial Work section

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -14,7 +14,7 @@ const Services = () => {
           duration-500'>
             <Image src={icon} alt='' className='w-10'/>
             <h3 className='text-lg my-4 text-gray-700'>{title}</h3>
-            <p className='text-sm text-gray-600 leading-5'>
+            <p className='text-[16px] text-gray-600 leading-5'>
               {description}
             </p>
             <a href={link} className='flex items-center gap-2 text-sm mt-5 font-semibold'>Read more 
