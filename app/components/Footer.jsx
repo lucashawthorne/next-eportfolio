f630bdc4c4cb2531963bd5d1b14602196b394dd6
Raindomize <lucashpro17@gmail.com>
2025-11-25 14:12:24 -0700

    implement initial footer

---
+++ app/components/Footer.jsx
@@ -0,0 +1,27 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+
+const Footer = () => {
+  return (
+    <div className='mt-20'>
+      <div className='text-center'>
+        <Image src={assets.logo} alt='' className='w-24 mx-auto mb-2'/>
+        <div className='w-max flex items-center gap-2 mx-auto'>
+          <Image src={assets.mail_icon} alt='' className='w-6'/>
+          <p className='font-semibold'>lucashawthorne16@gmail.com</p>
+        </div>
+      </div>
+      <div className='text-center sm:flex items-center justify-between border-t
+      border-gray-400 mx-[10%] mt-12 py-6'>
+        <p>© 2025 Lucas Hawthorne. All rights reserved.</p>
+        <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
+          <li><a href="https://github.com/lucashawthorne/" target='_blank'>GitHub</a></li>
+          <li><a href="https://linkedin.com/in/thelucashawthorne/" target='_blank'>LinkedIn</a></li>
+          <li><a href="/Lucas-Hawthorne's-Tech-Resume.pdf/" target='_blank'>Resume</a></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
